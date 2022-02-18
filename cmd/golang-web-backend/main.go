29c8afa5767fd679667ebc0f2d7394625a39ceff
marvin.deckmyn <marvin.deckmyn@student.howest.be>
2022-02-18 16:43:08 +0100

    Rewrite a REST api, not clean yet and storage is temporary atm

--- cmd/golang-web-backend/main.go
+++ cmd/golang-web-backend/main.go
@@ -3,31 +3,188 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
 )
 
-func main() {
-	fmt.Println("REST web server started")
+type Footballer struct {
+	ID           string `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	FootballClub string `json:"football_club"`
+}
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("/status was called")
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+type footballerHandlers struct {
+	sync.Mutex
+	store map[string]Footballer
+}
 
-		status := map[string]string{"status": "OK"}
-		json.NewEncoder(w).Encode(status)
-	})
-
-	http.HandleFunc("/", handleRoot)
-
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
+func (h *footballerHandlers) footballers(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
+		return
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+		return
 	}
 }
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	log.Println("/ was called")
-	w.WriteHeader(http.StatusNotImplemented)
+func (h *footballerHandlers) get(w http.ResponseWriter, r *http.Request) {
+	footballers := make([]Footballer, len(h.store))
+
+	h.Lock()
+	i := 0
+	for _, footballer := range h.store {
+		footballers[i] = footballer
+		i++
+	}
+	h.Unlock()
+
+	jsonBytes, err := json.Marshal(footballers)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *footballerHandlers) getRandomFootballer(w http.ResponseWriter, r *http.Request) {
+	ids := make([]string, len(h.store))
+	h.Lock()
+	i := 0
+	for id := range h.store {
+		ids[i] = id
+		i++
+	}
+	defer h.Unlock()
+
+	var target string
+	if len(ids) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if len(ids) == 1 {
+		target = ids[0]
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		target = ids[rand.Intn(len(ids))]
+	}
+
+	w.Header().Add("location", fmt.Sprintf("/footballers/%s", target))
+	w.WriteHeader(http.StatusFound)
+}
+
+func (h *footballerHandlers) getFootballer(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	if parts[2] == "random" {
+		h.getRandomFootballer(w, r)
+		return
+	}
+
+	h.Lock()
+	footballer, ok := h.store[parts[2]]
+	h.Unlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(footballer)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *footballerHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("Need content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+
+	var footballer Footballer
+	err = json.Unmarshal(bodyBytes, &footballer)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	footballer.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+	h.Lock()
+	h.store[footballer.ID] = footballer
+	defer h.Unlock()
+}
+
+func newFootballerHandlers() *footballerHandlers {
+	return &footballerHandlers{
+		store: map[string]Footballer{},
+	}
+}
+
+type adminPortal struct {
+	password string
+}
+
+func newAdminPortal() *adminPortal {
+	password := os.Getenv("ADMIN_PASSWORD")
+	if password == "" {
+		panic("required env var ADMIN_PASSWORD not set")
+	}
+
+	return &adminPortal{password: password}
+}
+
+func (a adminPortal) handler(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+	if !ok || user != "admin" || pass != a.password {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("401 - unauthorized"))
+		return
+	}
+
+	w.Write([]byte("<html><h1>Admin portal</h1></html>"))
+}
+
+func main() {
+	//admin := newAdminPortal()
+	footballerHandlers := newFootballerHandlers()
+	http.HandleFunc("/footballers", footballerHandlers.footballers)
+	http.HandleFunc("/footballers/", footballerHandlers.getFootballer)
+	//http.HandleFunc("/admin", admin.handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
